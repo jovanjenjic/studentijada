8760191084001649f3a13cbe1f2b4fc105293ef1
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2022-07-30 11:44:02 +0200

    Fix casing discrepancies.

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import React, { useContext, useState } from 'react';
+import { Layout } from '../components';
+import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+)};
+
+export default MyApp
