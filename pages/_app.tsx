8efa2c2f0c60731f80bd526bfc22314540a3a845
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2022-07-30 11:05:01 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import React, { useContext, useState } from 'react';
+import { Layout } from '../components';
 import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+)};
 
 export default MyApp
