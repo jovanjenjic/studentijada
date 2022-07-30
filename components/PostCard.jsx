a307125f825113ca46f56431e37a60ee45505e27
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2022-07-30 11:31:03 +0200

    Commit

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,36 +1,24 @@
 import React from 'react';
-// import Image from 'next/image';
+import Image from 'next/image';
 import moment from 'moment';
 import Link from 'next/link';
 
-// import { grpahCMSImageLoader } from '../util';
+import { grpahCMSImageLoader } from '../util';
 
 const PostCard = ({ post }) => (
   <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
-    {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
-      <Image
-        unoptimized
-        loader={grpahCMSImageLoader}
-        alt={post.title}
-        className="shadow-lg rounded-t-lg lg:rounded-lg"
-        layout="fill"
-        src={post.featuredImage.url}
-      />
-    </div> */}
     <div className="relative overflow-hidden shadow-md pb-80 mb-6">
       <img src={post.featuredImage.url} alt="" className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
     </div>
 
     <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
-      <Link href={`/post/${post.slug}`}>
-        {post.title}
-      </Link>
+      <Link href={`/post/${post.slug}`}>{post.title}</Link>
     </h1>
     <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
       <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
-        <img
-          // unoptimized
-          // loader={grpahCMSImageLoader}
+        <Image
+          unoptimized
+          loader={grpahCMSImageLoader}
           alt={post.author.name}
           height="30px"
           width="30px"
